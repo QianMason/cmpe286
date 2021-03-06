bd176211b330921a422b89f0daf8a5cbe0c7b925
Your Name <qian.mason@gmail.com>
2021-03-05 23:52:32 -0800

    unifying ssh functionality into server

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
-// import express (after npm install express)
 const express = require('express');
-
-// create new express app and save it as "app"
+const {NodeSSH} = require('node-ssh')
+const ssh = new NodeSSH()
 const app = express();
-
-// server configuration
 const PORT = 4000;
+app.use(express.json());
 
-// create a route for the app
+// ssh.connect({
+//   host: 'localhost',
+//   username: 'steel',
+//   privateKey: '/home/steel/.ssh/id_rsa'
+// })
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+// routes
 app.get('/', (req, res) => {
   res.send('Hello World');
   console.log("get request")
 })
-.post('/', (req, res) => {
-    command = req.body;
-    console.log(command);
+.post('/dispense', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log("post received");
+
+    ssh.connect({
+      host: 'localhost',
+      username: 'steel',
+      privateKey: '/home/steel/.ssh/id_rsa'
+    })
+    /*
+     Or
+     ssh.connect({
+       host: 'localhost',
+       username: 'steel',
+       privateKey: fs.readFileSync('/home/steel/.ssh/id_rsa', 'utf8')
+     })
+     if you want to use the raw string as private key
+     */
+    .then(function() {
+      // Local, Remote
+      ssh.execCommand('python motor.py', { cwd:'/documents/' }).then(function(result) {
+          
+          // response.writeHead(200);
+          // response.write('hello');
+          
+          // response.end();
+      }).catch(() =>{
+        console.log(error);
+          // response.writeHead(404);
+          // response.write('goodbye');
+          // response.end();
+      }
+      )
+      
+      // With streaming stdout/stderr callbacks
+    }).catch(() => {
+      // response.writeHead(500);
+      // response.write("Bad");
+      // response.end();
+    })
+
+    res.send("Thank you.");
 })
 
-// make the server listen to requests
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
+
